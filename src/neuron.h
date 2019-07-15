9833b1746e545eb86de9888724d65dbdaa892419
Stephen <stephenjhunt97@gmail.com>
2019-07-15 10:16:26 +0200

    Add Some Neuron.h helper funcs
    Implemented a few of the Neuron helper functions

--- src/neuron.h
+++ src/neuron.h
@@ -17,11 +17,39 @@ struct Neuron{
 	int Parent2;
 	int ID;
 	char* Name;
-	double* Weight;
+	double Weight[];
 };
 
 int counter = 0;
 //srand
+
+Neuron newNeuron(int size){
+	counter++;
+	Neuron n = new Neuron{};
+	n.ID = counter;
+	n.Weight = new double[size];
+	n.Name = "Neuron";
+	n.Parent1 = -1;
+	n.Parent2 = -1;
+	return n;
+}
+
+void createWeights(Neuron n, int size){
+	srand(time(0));
+	for(i=0;i<size;i++){
+		n.Weight[i] = (rand() * 12.0) - 6.0;
+	}
+}
+
+void setFitness(Neuron n, int fitness){
+	n.Fitness = n.Fitness + fitness;
+}
+
+void perturb(Neuron n, Neuron best){
+
+}
+
+
 
 
 
