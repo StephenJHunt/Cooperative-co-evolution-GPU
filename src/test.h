9fadc6545165b76099d1cc1f27d846bb34780e40
Stephen <stephenjhunt97@gmail.com>
2019-07-16 16:48:59 +0200

    Add tests for Population
    Debugging bansai!

--- src/test.h
+++ src/test.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <iostream>
 
 // includes CUDA
 #include <cuda_runtime.h>
@@ -33,4 +34,25 @@ void testSigmoid(){
 }
 void testPop(){
 	Population* p = newPopulation(10, 10);
+//	printf("am here\n");
+	Population deref = *p;
+//	Neuron nr = deref.Individuals[0];
+	createIndividuals(deref);
+	Neuron t = selectNeuron(deref);
+//	Neuron* testn = newNeuron(deref.GeneSize);
+//	printf("%f\n", testn->Weight[0]);
+//	createWeights(*testn, deref.GeneSize);
+//	deref.Individuals[0] = *testn;
+//	nr = deref.Individuals[0];
+//	createIndividuals(deref);
+//	Neurons ns = deref.Individuals;
+//	Neuron* n = ns.Neuron;
+//	Neuron nr0 = deref.Individuals[1];
+//	Neuron nr4 = deref.Individuals[3];
+//	Neuron nr1 = deref.Individuals[7];
+//	Neuron nr2 = deref.Individuals[8];
+//	Neuron nr3 = deref.Individuals[9];
+//	printf("%d\n%d\n%d\n%d\n%d\n%d\n%d",deref.NumToBreed, nr.Fitness, nr0.Fitness, nr4.Fitness, nr1.Fitness, nr2.Fitness, nr3.Fitness);
+//	printf("%f\n", nr.Weight[0]);
+	printf("test passed\n");
 }
