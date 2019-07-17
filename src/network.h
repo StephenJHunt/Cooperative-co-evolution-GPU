c30c102430ca89b4c865af1a1ea2c1bdd65bd272
Stephen <stephenjhunt97@gmail.com>
2019-07-17 14:48:59 +0200

    Add network interface
    Basically copy paste from Go implementation

--- src/network.h
+++ src/network.h
@@ -1,15 +1,26 @@
-/*
- * network.h
- *
- *  Created on: 15 Jul 2019
- *      Author: senpai
- */
-
 #ifndef NETWORK_H_
 #define NETWORK_H_
 
+#include "population.h"
+#include "neuron.h"
+
+class Network{
+	public:
+		double* Activate(double*, double*);
+		void Create(Population* p);
+		Neuron* getHiddenUnits();
+		int getTotalInputs();
+		int getTotalOutputs();
+		bool hasBias();
+		void setFitness(int);
+		int getFitness();
+		void setNeuronFitness();
+		void resetActivation();
+		void resetFitness();
+		void Tag();
+		int getID();
+};
 
 
 
-
-#endif /* NETWORK_H_ */
+#endif
