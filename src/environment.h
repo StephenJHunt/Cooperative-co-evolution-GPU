9e45b7597c4691d651ef6c5c71ffb679b922a402
Stephen <stephenjhunt97@gmail.com>
2019-07-17 15:50:14 +0200

    Add Environment interface
    Finally getting somewhere

--- src/environment.h
+++ src/environment.h
@@ -1,15 +1,29 @@
-/*
- * environment.h
- *
- *  Created on: 15 Jul 2019
- *      Author: senpai
- */
-
 #ifndef ENVIRONMENT_H_
 #define ENVIRONMENT_H_
 
+struct Gridworld{
+	int length;
+	int height;
+};
+
+struct State{
+	int* PredatorX;
+	int* PredatorY;
+	int PreyX;
+	int PreyY;
+};
+
+class Environment{
+public:
+	Gridworld* getWorld();
+	State* getState();
+	bool Caught();
+	bool Surrounded();
+	void PerformPreyAction(int);
+	void PerformPredatorAction(int);
+	void Reset(int);
+};
 
 
 
-
-#endif /* ENVIRONMENT_H_ */
+#endif
