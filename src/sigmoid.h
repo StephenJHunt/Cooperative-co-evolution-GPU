9d05ac17a12d7342804abb3178e79f901a91bbf1
Stephen <stephenjhunt97@gmail.com>
2019-07-29 08:57:53 +0200

    Rework activation
    Changed the output array to make sense

--- src/sigmoid.h
+++ src/sigmoid.h
@@ -9,7 +9,41 @@
 #include <time.h>
 
 double Logistic(double b, double t){
-	return (1/ (1 + exp(-(b*t))));
+	double res = (1/ (1 + exp(-(b*t))));
+	if(res < 1.0)return 0;
+	return res;
+}
+
+double Direction(double b){
+	/*
+	if(b > 0.0 && b <= 90.0){
+		return 0;
+	}
+	if(b > 90.0 && b <= 180.0){
+		return 1;
+	}
+	if(b > 180.0 && b <= 270.0){
+		return 2;
+	}
+	else{
+//	if(b > 315.0 || b <= 45.0){
+		return 3;
+	}*/
+
+	if(b > 45.0 && b <= 135.0){
+		return 0;
+	}
+	if(b > 135.0 && b <= 225.0){
+		return 1;
+	}
+	if(b > 225.0 && b <= 315.0){
+		return 2;
+	}
+	else{
+//	if(b > 315.0 || b <= 45.0){
+		return 3;
+	}
+	return -1;
 }
 
 #endif /* SIGMOID_H_ */
