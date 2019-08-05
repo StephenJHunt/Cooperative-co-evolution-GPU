26111d41273495e88ee780c6ef9d581e8efedcd4
Stephen <stephenjhunt97@gmail.com>
2019-08-05 15:31:40 +0200

    Change setPreyPositions to be device func
    Now GPU can call it

--- src/predatorprey.h
+++ src/predatorprey.h
@@ -38,11 +38,11 @@ void reset(PredatorPrey pp, int n){
 	pp.state->Caught = false;
 }
 
-__device__ void kernelReset(PredatorPrey pp, int n){
+//__device__ void kernelReset(PredatorPrey pp, int n){
+//
+//}
 
-}
-
-void setPreyPosition(PredatorPrey pp,int x, int y){
+__device__ void setPreyPosition(PredatorPrey pp,int x, int y){
 	pp.state->PreyX = x;
 	pp.state->PreyY = y;
 }
